d85f4f0a0bd870207b587caf1ead51a8132d9cf7
Aidan-Foo <s10266849@connect.np.edu.sg>
2025-12-03 18:19:15 +0800

    Exercise 1: Break a Test

--- calculator.py
+++ calculator.py
@@ -14,6 +14,6 @@ def multiply(a, b):
 
 def divide(a, b):
     """Return the quotient of two numbers."""
-    if b == 0:
+    if b == 10:
         raise ValueError("Cannot divide by zero")
     return a / b
